0219bbbd31af596877d1f67216a52816dd1b9a11
DarkGuy10 <62807269+DarkGuy10@users.noreply.github.com>
2024-01-21 00:46:50 +0530

    style: fix linting

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,11 +9,11 @@ const config: StorybookConfig = {
 		'../frontend/**/*.stories.@(js|jsx|mjs|ts|tsx)',
 	],
 	addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        '@storybook/addon-interactions',
-        '@storybook/addon-mdx-gfm'
-    ],
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-mdx-gfm',
+	],
 	framework: {
 		name: '@storybook/nextjs',
 		options: {},
